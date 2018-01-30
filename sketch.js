7c7bea9abae89a242cbb5780d5af76d9b8d9690b
Savion Triplett <striplett@wlapcs.org>
2018-01-30 19:09:38 +0000

    first commit

--- sketch.js
+++ sketch.js
@@ -1,8 +1,22 @@
 function setup(){
     createCanvas(600,400);
-    background(0);
 }
 
 function draw(){
-    
+   background(0); 
+   
+   loadPixels();
+   
+   for(var row = 0; row < height; row++){
+       for(var col = 0; col < width; col++){
+           var startingIndex = (col + row * width)*4;
+           
+           pixels[startingIndex] = 0; //red
+     pixels[startingIndex + 1] = 0; //green
+     pixels[startingIndex + 2] = col; //blue
+     pixels[startingIndex + 3] = 255; //alpha
+       }
+   }
+     
+     updatePixels();
 }
